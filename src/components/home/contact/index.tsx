cfe36799f21c2527af31ce5b38a3adfa80bdaf03
Iwobe <iwobe.aigboje@gmail.com>
2024-11-07 19:16:39 +0100

    contact success message update

--- src/components/home/contact/index.tsx
+++ src/components/home/contact/index.tsx
@@ -3,7 +3,6 @@ import { useEffect, useState } from 'react';
 import { useMediaQueries } from '../../../hooks/useMediaQueries';
 import { ButtonSize, ButtonVariant } from '../../../types';
 import ButtonLink from '../../common/buttons/button-link';
-import CustomHeading from '../../common/custom-heading';
 import ManageNotifications from './manage-notification';
 
 /**
@@ -75,13 +74,22 @@ const Contact = () => {
                   </svg>
                 </div>
 
-                <CustomHeading
-                  title="Thank you for your message!"
-                  paragraph="Sit tight—we’re already on it! Our team is excited to get back to you and will reach out as soon as we can. We can’t wait to connect!"
-                  size="large"
-                  className="text-white w-full text-center mx-auto mb-12"
-                  paragraphColor="text-white"
-                />
+                <div className="relative flex flex-col left-0 top-0 justify-center items-center">
+                  <h1
+                    className="mb-1 text-white font-bold font-neue text-2xl sm:text-4xl"
+                    data-aos="fade-up"
+                  >
+                    Thank you for your message!<span className="text-primary">.</span>
+                  </h1>
+                  <p
+                    className="mb-4 max-w-3xl text-base font-medium leading-relaxed sm:leading-relaxed sm:text-lg text-white mr-auto"
+                    data-aos="fade-up"
+                    data-aos-delay="200"
+                  >
+                    Sit tight—we&apos;re already on it! Our team is excited to get back to you and
+                    will reach out as soon as we can. We can’t wait to connect!
+                  </p>
+                </div>
               </div>
             </div>
           </div>
